ac2353aa8df48e6193e98ab19690aa042835c4c8
yucc1 <yucc1@lenovo.com>
2020-07-07 17:31:14 +0800

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='yucctools',
+        version='0.0.2',
+        description='useful tools, such as logger, time, vimrc',
+        url='',
+        author='Chen-Chen Yu',
+        author_email='6506666@gmail.com',
+        license='MIT',
+        packages=['yucctools'],
+        zip_safe=False)
+
